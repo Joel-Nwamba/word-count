f591066f60413f266983fa5c0983a93ee207b0e6
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2021-10-16 16:44:32 -0400

    updated chracter count

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const text = document.getElementById("text");
 const wordCount = document.getElementById("count");
+const characterCount = document.getElementById("character");
+const longCharacter = document.getElementById("longCharacter");
+let countCharacter = 0;
 let textValue = [];
 
 
@@ -10,14 +13,19 @@ function countCheck() {
     textValue.push(text.value);
     let stringJoin = textValue.join(" ");
     let stringArray =stringJoin.split(" ");
+    let character = stringJoin;
     let count = 0;
     for(let i = 0; i < stringArray.length; i++) {
         // console.log(stringArray[i])
         if(stringArray[i] != " ") {
             count++;
+            // countCharacter++;
         }
     }
     wordCount.textContent = count;
+    characterCount.textContent = text.value.split("").length; 
+    longCharacter.textContent = character;
+
 }
 
 
